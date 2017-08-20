dfed7246ef834a02f80eecaf1ddc35873920e064
Ryan Brott <rcbrott@gmail.com>
2017-08-19 21:56:20 -0700

    Add basic expand/collapse functionality to config option groups

--- dashboard/src/containers/ConfigOptionGroup.jsx
+++ dashboard/src/containers/ConfigOptionGroup.jsx
@@ -2,22 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ConfigOption from './ConfigOption';
 import Heading from '../components/Heading';
+import Icon from '../components/Icon';
 
-const ConfigOptionGroup = ({ name, options }) => (
-  <div>
-    <Heading level={3} text={name} />
-    <table>
-      <tbody>
-        {options.map((option, optionIndex) => (
-          <ConfigOption
-            key={optionIndex}
-            optionGroup={name}
-            option={option} />
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+class ConfigOptionGroup extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: true
+    };
+
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+
+  toggleVisibility() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Heading level={3} text={this.props.name} >
+          <Icon icon={ this.state.expanded ? 'remove' : 'add' } size='small' onClick={this.toggleVisibility} />
+        </Heading>
+        {
+          this.state.expanded ?
+            (
+              <table>
+                <tbody>
+                  {this.props.options.map((option, optionIndex) => (
+                    <ConfigOption
+                      key={optionIndex}
+                      optionGroup={this.props.name}
+                      option={option} />
+                  ))}
+                </tbody>
+              </table>
+            )
+            : null
+        }
+      </div>
+    );
+  }
+}
 
 ConfigOptionGroup.propTypes = {
   name: PropTypes.string.isRequired,
