387056688ba05c2d8edebb9c4d089f845f214124
Ryan Brott <rcbrott@gmail.com>
2017-08-21 22:25:51 -0700

    Add basic Camera2 support based on Google's sample

---
+++ TeamCode/src/main/java/com/acmerobotics/code/CameraTest.java
@@ -0,0 +1,43 @@
+package com.acmerobotics.code;
+
+import android.app.Activity;
+import android.media.ImageReader;
+import android.util.Log;
+
+import com.acmerobotics.library.camera2.Camera2Fragment;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.R;
+
+/**
+ * Created by ryanbrott on 8/21/17.
+ */
+
+@TeleOp(name = "Camera Test")
+public class CameraTest extends OpMode {
+
+    @Override
+    public void init() {
+        ((Activity) hardwareMap.appContext).getFragmentManager().beginTransaction()
+                .replace(R.id.cameraMonitorViewId, Camera2Fragment.newInstance(new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader imageReader) {
+                        Log.i("CameraTest", "onImageAvailable");
+                        // pretend to consume the image
+                        imageReader.acquireLatestImage().close();
+                    }
+                }, R.layout.fragment_camera2))
+                .commit();
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
