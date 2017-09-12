c5053e712b5da693abd9401ac6d3a7f01a5cc8f0
rbrott <rcbrott@gmail.com>
2017-09-11 18:23:39 -0700

    Add proper support for Vuforia to OpenCV image conversion

--- TeamCode/src/main/java/com/acmerobotics/relicrecovery/opmodes/VuMarkIdentification.java
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/opmodes/VuMarkIdentification.java
@@ -39,7 +39,6 @@ import com.vuforia.Image;
 import com.vuforia.PIXEL_FORMAT;
 import com.vuforia.Vuforia;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaNavigation;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
@@ -48,16 +47,17 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -66,25 +66,6 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
-
-/**
- * This OpMode illustrates the basics of using the Vuforia engine to determine
- * the identity of Vuforia VuMarks encountered on the field. The code is structured as
- * a LinearOpMode. It shares much structure with {@link ConceptVuforiaNavigation}; we do not here
- * duplicate the core Vuforia documentation found there, but rather instead focus on the
- * differences between the use of Vuforia for navigation vs VuMark identification.
- *
- * @see ConceptVuforiaNavigation
- * @see VuforiaLocalizer
- * @see VuforiaTrackableDefaultListener
- * see  ftc_app/doc/tutorial/FTC_FieldCoordinateSystemDefinition.pdf
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
- *
- * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
- * is explained in {@link ConceptVuforiaNavigation}.
- */
 
 @Autonomous(name="VuMark Id", group ="Concept")
 public class VuMarkIdentification extends LinearOpMode {
@@ -96,11 +77,6 @@ public class VuMarkIdentification extends LinearOpMode {
     private byte[] imgData;
     private CountDownLatch openCvInitialized = new CountDownLatch(1);
     private File imageSaveLocation;
-
-    /**
-     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
-     * localization engine.
-     */
     VuforiaLocalizer vuforia;
 
     BlockingQueue<VuforiaLocalizer.CloseableFrame> frameQueue;
@@ -111,7 +87,7 @@ public class VuMarkIdentification extends LinearOpMode {
         imageSaveLocation = new File(Environment.getExternalStorageDirectory(), "ACME");
         imageSaveLocation.mkdirs();
 
-        BaseLoaderCallback loaderCallback = new BaseLoaderCallback(hardwareMap.appContext) {
+        final BaseLoaderCallback loaderCallback = new BaseLoaderCallback(hardwareMap.appContext) {
             @Override
             public void onManagerConnected(int status) {
                 switch (status) {
@@ -128,57 +104,32 @@ public class VuMarkIdentification extends LinearOpMode {
             }
         };
 
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, hardwareMap.appContext, loaderCallback);
+        AppUtil.getInstance().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, hardwareMap.appContext, loaderCallback);
+            }
+        });
 
-        Vuforia.setFrameFormat(PIXEL_FORMAT.GRAYSCALE, false);
-        Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
-
-        /*
-         * To start up Vuforia, tell it the view that we wish to use for camera monitor (on the RC phone);
-         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
-         */
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
         // OR...  Do Not Activate the Camera Monitor View, to save power
         // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
-        /*
-         * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
-         * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
-         * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
-         * web site at https://developer.vuforia.com/license-manager.
-         *
-         * Vuforia license keys are always 380 characters long, and look as if they contain mostly
-         * random data. As an example, here is a example of a fragment of a valid key:
-         *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
-         * Once you've obtained a license key, copy the string from the Vuforia web site
-         * and paste it in to your code onthe next line, between the double quotes.
-         */
         parameters.vuforiaLicenseKey = "AaNzdGn/////AAAAGVCiwQaxg01ft7Lw8kYMP3aE00RU5hyTkE1CNeaYi16CBF0EC/LWi50VYsSMdJITYz6jBTmG6UGJNaNXhzk1zVIggfVmGyEZFL5doU6eVaLdgLyVmJx6jLgNzSafXSLnisXnlS+YJlCaOh1pwk08tWM8Oz+Au7drZ4BkO8j1uluIkwiewRu5zDZGlbNliFfYeCRqslBEZCGxiuH/idcsD7Q055Bwj+f++zuG3x4YlIGJCHrTpVjJUWEIbdJzJVgukc/vVOz21UNpY6WoAwH5MSeh4/U6lYwMZTQb4icfk0o1EiBdOPJKHsxyVF9l00r+6Mmdf6NJcFTFLoucvPjngWisD2T/sjbtq9N+hHnKRpbK\n";
-
-        /*
-         * We also indicate which camera on the RC that we wish to use.
-         * Here we chose the back (HiRes) camera (for greater range), but
-         * for a competition robot, the front camera might be more convenient.
-         */
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         parameters.useExtendedTracking = false;
         parameters.fillCameraMonitorViewParent = true;
         this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
         vuforia.setFrameQueueCapacity(10);
 
         frameQueue = vuforia.getFrameQueue();
 
-        /**
-         * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
-         * in this data set: all three of the VuMarks in the game were created from this one template,
-         * but differ in their instance id information.
-         * @see VuMarkInstanceId
-         */
         VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+        VuforiaTrackable relicVuMark = relicTrackables.get(0);
+        relicVuMark.setName("relicVuMark");
 
         openCvInitialized.await();
 
@@ -189,25 +140,12 @@ public class VuMarkIdentification extends LinearOpMode {
         relicTrackables.activate();
 
         while (opModeIsActive()) {
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicVuMark);
 
-            /**
-             * See if any of the instances of {@link relicTemplate} are currently visible.
-             * {@link RelicRecoveryVuMark} is an enum which can have the following values:
-             * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
-             * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
-             */
-            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
             if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-
-                /* Found an instance of the template. In the actual game, you will probably
-                 * loop until this condition occurs, then move on to act accordingly depending
-                 * on which VuMark was visible. */
                 telemetry.addData("VuMark", "%s visible", vuMark);
 
-                /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
-                 * it is perhaps unlikely that you will actually need to act on this pose information, but
-                 * we illustrate it nevertheless, for completeness. */
-                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicVuMark.getListener()).getPose();
                 telemetry.addData("Pose", format(pose));
 
                 /* We further illustrate how to decompose the pose into useful rotational and
@@ -226,8 +164,7 @@ public class VuMarkIdentification extends LinearOpMode {
                     double rY = rot.secondAngle;
                     double rZ = rot.thirdAngle;
                 }
-            }
-            else {
+            } else {
                 telemetry.addData("VuMark", "not visible");
             }
 
@@ -235,35 +172,28 @@ public class VuMarkIdentification extends LinearOpMode {
             telemetry.addData("num_frames", frameQueue.size());
             if (!frameQueue.isEmpty()) {
                 VuforiaLocalizer.CloseableFrame frame = frameQueue.take();
-                Log.i(TAG, "Got frame with " + frame.getNumImages() + " images");
                 for (int i = 0; i < frame.getNumImages(); i++) {
                     Image image = frame.getImage(i);
-                    Log.i(TAG, "Got image of format: " + image.getFormat());
-                    ByteBuffer byteBuffer = image.getPixels();
-                    if (imgData == null || imgData.length != byteBuffer.capacity()) {
-                        imgData = new byte[byteBuffer.capacity()];
-                    }
-                    byteBuffer.get(imgData);
                     if (image.getFormat() == PIXEL_FORMAT.RGB565) {
+                        ByteBuffer byteBuffer = image.getPixels();
+                        if (imgData == null || imgData.length != byteBuffer.capacity()) {
+                            imgData = new byte[byteBuffer.capacity()];
+                        }
+                        byteBuffer.get(imgData);
                         if (raw == null || raw.width() != image.getWidth() || raw.height() != image.getHeight()) {
                             raw = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC2);
+                            rgb = new Mat();
                         }
                         raw.put(0, 0, imgData);
-                        Log.i(TAG, "raw is " + raw);
-                        Log.i(TAG, "image is " + image);
-                        rgb = new Mat();
                         Imgproc.cvtColor(raw, rgb, Imgproc.COLOR_BGR5652BGR);
 
                         // do things
+                        MatOfInt params = new MatOfInt();
+                        params.fromArray(Imgcodecs.CV_IMWRITE_JPEG_QUALITY, 100);
+                        Imgcodecs.imwrite(new File(imageSaveLocation, System.currentTimeMillis() + "_" + i + ".jpg").getPath(), rgb, params);
 
                         rgb.release();
                         raw.release();
-                    } else if (image.getFormat() == PIXEL_FORMAT.GRAYSCALE) {
-                        if (raw == null || raw.width() != image.getWidth() || raw.height() != image.getHeight()) {
-                            raw = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8U);
-                        }
-                        raw.put(0, 0, imgData);
-                        Imgcodecs.imwrite(new File(imageSaveLocation, System.currentTimeMillis() + "_" + i + ".jpg").getPath(), raw);
                     }
                 }
                 frame.close();
